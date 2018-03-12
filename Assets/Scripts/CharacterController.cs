20b7166728b10386afa5fc50faec65a721cb82aa
Adam Waśniewski <tabuplaw@gmail.com>
2018-03-12 10:21:36 +0100

    First commit

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+    [SerializeField] float moveSpeed = 1;
+
+
+    private Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if(Input.GetKey("w"))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + 1 * moveSpeed * Time.deltaTime, transform.position.z);
+            anim.SetBool("isWalking", true);
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.position = new Vector3(transform.position.x - 1*moveSpeed*Time.deltaTime, transform.position.y, transform.position.z);
+            anim.SetBool("isWalking", true);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - 1 * moveSpeed * Time.deltaTime, transform.position.z);
+            anim.SetBool("isWalking", true);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.position = new Vector3(transform.position.x + 1 * moveSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+            anim.SetBool("isWalking", true);
+        }
+
+        else
+            anim.SetBool("isWalking", false);
+    }
+}
---
+++ Assets/Scripts/WorldGenerator.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGenerator : MonoBehaviour {
+
+    [SerializeField] float tileSize = 0.64f;
+    [SerializeField] GameObject[] tiles;
+    [SerializeField] GameObject[] objects;
+
+
+    private List<GameObject> tileList = new List<GameObject>();
+    private List<GameObject> objectsList = new List<GameObject>();
+	// Use this for initialization
+	void Start () {
+		for(int x = 0; x< 256; x++)
+        {
+            for(int y = 0; y<256; y++)
+            {
+                int randomObject = Random.Range(0, 100);
+                if (randomObject > 97)
+                    createObject(x, y);
+                createTile(x, y);
+            }
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void createTile(int x, int y)
+    {
+        int whichTile = Random.Range(0, tiles.Length);
+        GameObject newTile = Instantiate(tiles[whichTile]);
+        newTile.transform.position = new Vector2(x * tileSize, y*tileSize);
+        tileList.Add(newTile);
+
+    }
+
+    private void createObject(int x, int y)
+    {
+        int whichObject = Random.Range(0, objects.Length-1);
+        GameObject newObject = Instantiate(objects[whichObject]);
+        newObject.transform.position = new Vector2(x * tileSize, y * tileSize);
+        objectsList.Add(newObject);
+    }
+}
